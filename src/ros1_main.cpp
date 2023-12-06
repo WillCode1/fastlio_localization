8d0aca089780a8edf1d1a02b9823711e514de749
will <2623556566@qq.com>
2023-12-06 18:21:06 +0800

    asd

--- src/ros1_main.cpp
+++ src/ros1_main.cpp
@@ -129,7 +129,7 @@ void publish_odometry(const ros::Publisher &pubOdomAftMapped, const state_ikfom 
 
     if (std::abs(baselink_pos.x()) > 1e7 || std::abs(baselink_pos.y()) > 1e7 || std::abs(baselink_pos.z()) > 1e7)
     {
-        LOG_WARN("localization state maybe valid! (imu frame) pos(%f, %f, %f)", baselink_pos.x(), baselink_pos.y(), baselink_pos.z());
+        LOG_WARN("localization state maybe valid! (baselink frame) pos(%f, %f, %f)", baselink_pos.x(), baselink_pos.y(), baselink_pos.z());
     }
 
     nav_msgs::Odometry odomAftMapped;
